c664dae96a027d6376a297a205219ac372df9736
0xhimangshu <saikiahimangshu1256@gmail.com>
2024-12-05 20:59:42 +0530

    updated

--- background.js
+++ background.js
@@ -19,4 +19,35 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   
         return true;
     }
+    
+    if (request.type === 'SUMMARIZE_TEXT') {
+        const text = request.text;
+        
+        fetch('https://api.meaningcloud.com/summarization-1.0', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            },
+            body: new URLSearchParams({
+                key: 'demo', // Using demo key for free tier
+                txt: text,
+                sentences: '1'
+            })
+        })
+        .then(response => response.json())
+        .then(data => {
+            sendResponse({
+                success: true,
+                summary: data.summary
+            });
+        })
+        .catch(error => {
+            sendResponse({
+                success: false,
+                error: error.message
+            });
+        });
+
+        return true;
+    }
 });
